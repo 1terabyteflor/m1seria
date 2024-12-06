7e6abdfba339a8707a59b4513f1358de8f34869f
1TB <florencgmz@gmail.com>
2024-12-06 17:12:42 -0300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,19 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="w-full h-screen p-4  flex flex-col justify-between items-center">
-      <p className="font-bold self-start text-lg md:text-2xl">m1seria</p>
+    <div className="w-full h-screen p-4 flex flex-col justify-between items-center">
+      <p className="font-bold self-start text-xl md:text-2xl">m1seria</p>
       <div className="flex-grow w-full flex flex-col justify-center items-center">
         <Image 
           src="/rojo.jpeg"
-          alt="M1SERIA"
+          alt="m2seria"
           width={300}
           height={225}
           className="object-cover"
         />
-        <p className="font-bold mt-2 text-lg md:text-xl">M1SERIA V2</p>
+        <p className="font-bold mt-2 text-lg md:text-xl">M2SERIA</p>
         <p className="text-md md:text-lg">$ xxx</p>
-        <p className="border h-fit border-black font-[family-name:var(--einaBold)] px-4 py-2 text-center mt-2 cursor-pointer hover:bg-black hover:border-black hover:text-white">
+        <p className="border border-black font-[family-name:var(--einaBold)] w-[300px] h-fit py-2 flex items-center justify-center text-center mt-2 cursor-pointer hover:bg-black hover:border-black hover:text-white">
           comprar
         </p>
       </div>
